442f61ad6a4898caf7fadf85423b61985a3d1ee4
zahash <zahash.z@gmail.com>
2023-07-17 22:04:24 +0530

    type hints with generics

--- amnis/stream.py
+++ amnis/stream.py
@@ -11,18 +11,22 @@ Grouper = NamedTuple(
     ]
 )
 
+T = TypeVar("T")
+U = TypeVar("U")
+V = TypeVar("V")
 
-class Stream:
-    def __init__(self, iterable: Iterable):
-        self._iterator = iter(iterable)
 
-    def __iter__(self):
+class Stream(Generic[T]):
+    def __init__(self, iterable: Iterable[T]):
+        self._iterator: Iterator[T] = iter(iterable)
+
+    def __iter__(self) -> Iterator[T]:
         return self._iterator
 
-    def __next__(self):
+    def __next__(self) -> T:
         return next(self._iterator)
 
-    def apply(self, fn: Callable[[Iterable], Iterable]) -> "Stream":
+    def apply(self, fn: Callable[[Iterable[T]], Iterable[U]]) -> "Stream[U]":
         return Stream(fn(self))
 
     def catch(self, handler: Callable[["Exception"], Any], err_type=Exception) -> "Stream":
@@ -39,24 +43,24 @@ class Stream:
 
         return self.apply(_catch)
 
-    def map(self, fn: Callable[[Any], Any]) -> "Stream":
+    def map(self, fn: Callable[[T], U]) -> "Stream[U]":
         return self.apply(partial(map, fn))
 
-    def filter(self, fn: Callable[[Any], bool]) -> "Stream":
+    def filter(self, fn: Callable[[T], bool]) -> "Stream[T]":
         return self.apply(partial(filter, fn))
 
-    def flatten(self) -> "Stream":
-        def _flatten(nested_iterable: Iterable) -> Iterable:
+    def flatten(self) -> "Stream[Iterable[T]]":
+        def _flatten(nested_iterable: Iterable[Iterable[T]]) -> Iterable[T]:
             for iterable in nested_iterable:
                 yield from iterable
 
         return self.apply(_flatten)
 
-    def flatmap(self, fn: Callable[[Any], Iterable]) -> "Stream":
+    def flatmap(self, fn: Callable[[T], Iterable[U]]) -> "Stream[U]":
         return self.map(fn).flatten()
 
-    def distinct(self) -> "Stream":
-        def _distinct(iterable: Iterable) -> Iterable:
+    def distinct(self) -> "Stream[T]":
+        def _distinct(iterable: Iterable[T]) -> Iterable[T]:
             seen = set()
             for item in iterable:
                 if item not in seen:
@@ -65,11 +69,11 @@ class Stream:
 
         return self.apply(_distinct)
 
-    def sorted(self) -> "Stream":
+    def sorted(self) -> "Stream[T]":
         return self.apply(sorted)
 
-    def limit(self, n: int) -> "Stream":
-        def _limit(iterable: Iterable, n: int) -> Iterable:
+    def limit(self, n: int) -> "Stream[T]":
+        def _limit(iterable: Iterable[T], n: int) -> Iterable[T]:
             for item in iterable:
                 if n == 0:
                     break
@@ -78,8 +82,8 @@ class Stream:
 
         return self.apply(partial(_limit, n=max(n, 0)))
 
-    def skip(self, n: int) -> "Stream":
-        def _skip(iterable: Iterable, n: int) -> Iterable:
+    def skip(self, n: int) -> "Stream[T]":
+        def _skip(iterable: Iterable[T], n: int) -> Iterable[T]:
             for item in iterable:
                 if n == 0:
                     yield item
@@ -89,8 +93,8 @@ class Stream:
 
         return self.apply(partial(_skip, n=max(n, 0)))
 
-    def takeuntil(self, fn: Callable[[Any], bool]) -> "Stream":
-        def _takeuntil(iterable: Iterable) -> Iterable:
+    def takeuntil(self, fn: Callable[[T], bool]) -> "Stream[T]":
+        def _takeuntil(iterable: Iterable[T]) -> Iterable[T]:
             for item in iterable:
                 if not fn(item):
                     break
@@ -98,8 +102,8 @@ class Stream:
 
         return self.apply(_takeuntil)
 
-    def skipuntil(self, fn: Callable[[Any], bool]) -> "Stream":
-        def _skipuntil(iterable: Iterable) -> Iterable:
+    def skipuntil(self, fn: Callable[[T], bool]) -> "Stream[T]":
+        def _skipuntil(iterable: Iterable[T]) -> Iterable[T]:
             for item in iterable:
                 if not fn(item):
                     yield item
@@ -109,16 +113,16 @@ class Stream:
 
         return self.apply(_skipuntil)
 
-    def first(self) -> Optional[Any]:
+    def first(self) -> Optional[T]:
         return self.nth(0)
 
-    def nth(self, n: int) -> Optional[Any]:
+    def nth(self, n: int) -> Optional[T]:
         for item in self:
             if n == 0:
                 return item
             n -= 1
 
-    def last(self) -> Optional[Any]:
+    def last(self) -> Optional[T]:
         last_item = None
         for item in self:
             last_item = item
@@ -127,26 +131,26 @@ class Stream:
     def collect(self, collector: Callable[[Iterable], Iterable] = iter) -> Union[Sequence, Iterable, AbstractSet]:
         return collector(self)
 
-    def reduce(self, fn: Callable[[Any, Any], Any], initial=None) -> Optional[Any]:
+    def reduce(self, fn: Callable[[T, T], T], initial=None) -> Optional[T]:
         with suppress(TypeError):  # thrown when stream is empty without initial value
             return reduce(fn, self, initial) if initial is not None else reduce(fn, self)
 
-    def max(self, key: Callable[[Any], Any] = lambda x: x) -> Optional[Any]:
+    def max(self, key: Callable[[T], U] = lambda x: x) -> Optional[T]:
         with suppress(ValueError):  # thrown when stream is empty
             return max(self, key=key)
 
-    def min(self, key: Callable[[Any], Any] = lambda x: x) -> Optional[Any]:
+    def min(self, key: Callable[[T], U] = lambda x: x) -> Optional[T]:
         with suppress(ValueError):  # thrown when stream is empty
             return min(self, key=key)
 
-    def find(self, fn: Callable[[Any], bool]) -> Optional[Any]:
+    def find(self, fn: Callable[[T], bool]) -> Optional[T]:
         return self.filter(fn).first()
 
-    def foreach(self, fn: Callable[[Iterable], None]) -> None:
+    def foreach(self, fn: Callable[[T], None]) -> None:
         for item in self:
             fn(item)
 
-    def group(self, key_fn: Callable[[Any], Any], val_fn: Callable[[Any], Any], grouper: Grouper) -> dict:
+    def group(self, key_fn: Callable[[T], U], val_fn: Callable[[T], V], grouper: Grouper) -> dict:
         d = defaultdict(grouper.collection)
         for item in self:
             key, val = key_fn(item), val_fn(item)
@@ -159,10 +163,10 @@ class Stream:
                 d[key] = return_val
         return dict(d)
 
-    def allmatch(self, fn: Callable[[Any], bool]) -> bool:
+    def allmatch(self, fn: Callable[[T], bool]) -> bool:
         return all(fn(item) for item in self)
 
-    def anymatch(self, fn: Callable[[Any], bool]) -> bool:
+    def anymatch(self, fn: Callable[[T], bool]) -> bool:
         return any(fn(item) for item in self)
 
     def count(self) -> int:
